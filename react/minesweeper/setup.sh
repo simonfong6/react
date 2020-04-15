12bf921c00a2fe5dc15bcf0cef182c8c672de4f9
Simon <simonfong6@gmail.com>
2020-04-15 14:52:42 -0700

    stuff

---
+++ react/minesweeper/setup.sh
@@ -0,0 +1,13 @@
+# Initialize Package.json
+npm init --yes
+
+# Install packages.
+npm install --save \
+    webpack \
+    webpack-cli \
+    @babel/core \
+    @babel/preset-env \
+    @babel/preset-react \
+    babel-loader \
+    react \
+    react-dom
